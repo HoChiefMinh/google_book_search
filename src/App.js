7b8d068e3b424e9f8203114c9744773de216a5e4
Minh Pham <mqpham848@gmail.com>
2019-10-25 17:26:40 -0400

    built search UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <form>
+        <label> 
+          <span>Search for books</span>
+          <input
+            type='search'
+            placeholder='Insert Book'
+          />
+          <button type='submit'>Search</button>
+        </label>
+      </form>
+    </section>
   );
 }
 
