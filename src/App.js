3806b92a113dfc1aa8c2e6fb4ade927c37aba93a
Minh Pham <mqpham848@gmail.com>
2019-10-26 00:23:34 -0400

    api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import SearchForm from './components/SearchForm.js';
 
+
 function App () {
   return <SearchForm />;
 }
---
+++ utils/API.js
@@ -0,0 +1,4 @@
+import axios from 'axios';
+const BASEURL = 'https://www.google.com/books/jsapi.js';
+const APIKEY = 'process.env.REACT_APP_API_KEY';
+
