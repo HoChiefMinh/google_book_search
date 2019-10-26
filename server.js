1bf8277f33ef198424be3153678d222ea08d6efd
Minh Pham <mqpham848@gmail.com>
2019-10-26 08:03:26 -0400

    server.js

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 const app = express();
-const mongojs = require("mongojs");
+const mongojs = require('mongojs');
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 }
 
 // Define API routes here
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://HoChiefMinh:<Homework>@googlebooks-fuwby.mongodb.net/test?retryWrites=true&w=majority");
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 app.listen(PORT, () => {
