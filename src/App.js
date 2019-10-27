fe4278893de652e403a323b1b15183607597cab4
Minh Pham <mqpham848@gmail.com>
2019-10-26 22:14:07 -0400

    building search.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Nav from './components/NavBs';
 import './App.css';
-import SearchForm from './components/SearchForm.js';
+import BookSearch from './pages/BookSearch';
+// import SearchForm from './components/SearchForm.js';
+// import { Navbar, Brand, Container } from 'react-bootstrap';
 
 
 function App () {
-  return <SearchForm />;
+  return (
+    // <Router>
+      <div>
+        <Nav />
+        <BookSearch />
+      </div>
+    // </Router>
+    );
 }
 
 export default App;
