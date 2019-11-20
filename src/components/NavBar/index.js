0150bb23ee892dbcd11de973a2860d1a0ead4a7c
Minh Pham <mqpham848@gmail.com>
2019-11-20 14:21:08 +0900

    adding stuff

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './NavBar.css';
+
+function Nav() {
+  return (
+    <nav className='navbar navbar-dark bg-dark'>
+      <div class='container'>
+        <a class='navbar-brand' href='#'>
+          Navbar
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
